c2b914fe5dfd7be876cc1fae13848339c0e27d23
Gar <gar@tut.by>
2022-01-26 10:26:39 +0300

    fix: solve morse task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        let subString = expr.substring(i, i+10);
+      subString = subString.replace(/10/g, '.');
+      subString = subString.replace(/11/g, '-');
+      subString = subString.replace(/0/g, '');
+      subString = subString.replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ');
+      if (subString !== ' ') {
+          subString = subString.replace(subString, MORSE_TABLE[subString])
+      }
+      arr.push(subString);
+    }
+    return arr.join('')
+  }
 
 module.exports = {
     decode
